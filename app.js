e46caacf4cf1af8b484d23282ca68cea68670937
Ali <ali.frankel@gmail.com>
2023-02-26 15:47:49 -0500

    initial setup

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
+// app.use((req, res, next) => {
+//   console.log()
+//   next()
+// });
+
 const PORT = 1337;
 
 app.listen(PORT, () => {
